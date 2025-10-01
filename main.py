036f19f815a432f0016b783299270321c0a67f50
BigBadWolf <86110220+Warrikard@users.noreply.github.com>
2025-10-01 13:44:16 +0000

    exo done final

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from math import sqrt
 def isprime(p):
     if p < 2:
         return False
-    for i in range (2, int(sqrt(p))):
+    for i in range(2, int(sqrt(p)) + 1):
         if p % i == 0:
             return False
     return True
