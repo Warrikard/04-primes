847e06baa25a00bb630fec3b2c71c33dc9178ffd
Mike - CUNHA <86110220+Warrikard@users.noreply.github.com>
2025-10-08 13:51:52 +0000

    code fini

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 #### Fonction secondaire
 
+import string
 
 def ispalindrome(p):
+    # Met en minuscules
+    p = p.lower()
+    # Supprime les espaces
+    p = p.replace(" ", "")
+    # Supprime la ponctuation
+    punct = string.punctuation
+    for c in punct:
+        p = p.replace(c, "")
+    # Supprime les accents
+    #dic = {'é': 'e', 'è': 'e', 'ê': 'e', 'ë': 'e', 'à': 'a', 'â': 'a', 'ù': 'u', 'û': 'u', 'ô': 'o', 'î': 'i', 'ï': 'i', 'ç': 'c'}
+    p = p.translate(str.maketrans('éèêëàâùûôîïç', 'eeeeaauuoiic'))
 
-    # votre code ici
-    
+    # Vérifie si le mot est un palindrome
+    if p == p[::-1]:
+        return True
     return False
+
 
 #### Fonction principale
 
